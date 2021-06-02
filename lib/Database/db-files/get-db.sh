37420819345d9ba757c4c245e7b5c45d525aef2f
bridystone <github@breidenstein.info>
2021-06-02 16:41:05 +0200

    comment icons shown for TT

--- lib/Database/db-files/get-db.sh
+++ lib/Database/db-files/get-db.sh
@@ -1,1 +1,1 @@
-./adb shell "run-as info.breidenstein.rock_carrot cat /data/user/0/info.breidenstein.rock_carrot/databases/sandsteinklettern.db" > sandsteinklettern.db
+adb shell "run-as info.breidenstein.rock_carrot cat /data/user/0/info.breidenstein.rock_carrot/databases/sandsteinklettern.db" > sandsteinklettern.db
--- lib/Database/sqlRoutes.dart
+++ lib/Database/sqlRoutes.dart
@@ -6,14 +6,26 @@ extension SqlHandlerRoutes on SqlHandler {
     int gipfelid,
   ) {
     return database.then((db) => db.rawQuery(
-          'SELECT'
-          ' wege.*,'
-          ' COUNT(DISTINCT komment.komment_id) as komment_count'
-          ' FROM wege'
-          ' LEFT OUTER JOIN komment'
-          ' ON wege.weg_id = komment.wegid'
-          ' WHERE wege.gipfelid = ?'
-          ' GROUP BY wegnr',
+          '''
+          SELECT
+            wege.*,
+            COUNT(DISTINCT komment.komment_id) + 
+            COUNT(DISTINCT tt_comments.id) as comment_count
+            FROM wege
+
+            -- sandstein comments
+            LEFT OUTER JOIN komment
+            ON wege.weg_id = komment.wegid
+
+            -- tt comments if available
+            LEFT JOIN tt_mapping_routes
+            ON tt_mapping_routes.sandstein_routeid = wege.weg_id
+            LEFT JOIN tt_comments
+            ON tt_comments.routeid = tt_mapping_routes.tt_routeid
+
+          WHERE wege.gipfelid = ?
+          GROUP BY wegnr
+          ''',
           [gipfelid],
         ));
   }
